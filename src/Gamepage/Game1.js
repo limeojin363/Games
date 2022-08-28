a11dd47d3a74cc4aeb09863f0a6eeff2116ccb9e
John Doeexit <2002eojin@gmail.com>
2022-08-29 00:19:48 +0900

    Game1 약간

--- src/Gamepage/Game1.js
+++ src/Gamepage/Game1.js
@@ -4,13 +4,23 @@ function Game1() {
     return (
         <div className={styles.game1}>
             <div className={styles.container}>
-                <div className={styles.item}>
-                    <div className={styles.iteminside}>
-                        asdfasd
+                <div>
+                    <div className={styles.item + ' ' + styles.left}>
+                        <div className={styles.main}>
+                            <input>
+                            </input>
+                            <input>
+                            </input>
+                            <input>
+                            </input>
+                        </div>
+                        <div className={styles.res}>
+                            asdfadsadsfafsd
+                        </div>
                     </div>
                 </div>
                 <div className={styles.item}>
-
+                    <h5 className={styles.record}>Record</h5>
                 </div>
             </div>
         </div>
